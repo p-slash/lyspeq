b446c27d27c03306a228a6472b984929e0ce491b
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2018-05-14 17:43:36 -0400

    integrate q matrix expressions

---
+++ gsltools/integrator.cpp
@@ -0,0 +1,120 @@
+#include "integrator.hpp"
+
+#include <new>
+
+#define WORKSPACE_SIZE 3000
+#define TABLE_SIZE 100
+#define GL_INFINTY 1E10
+
+#define ABS_ERROR 0
+#define REL_ERROR 1E-8
+
+Integrator::Integrator( GSL_TYPE gsl_type_key, \
+						double (*integrand_function)(double, void *), void *params)
+{
+	gsl_type = gsl_type_key;
+
+	if (gsl_type == GSL_GL)
+	{
+		t = gsl_integration_glfixed_table_alloc(TABLE_SIZE);
+
+		if (t == NULL)
+		{
+			throw std::bad_alloc();
+		}
+	}
+
+	else
+	{
+		w = gsl_integration_workspace_alloc(WORKSPACE_SIZE);
+
+		if (w == NULL)
+		{
+			throw std::bad_alloc();
+		}
+	}
+
+  	F.function = integrand_function;
+  	F.params = params;
+}
+
+Integrator::~Integrator()
+{
+	if (gsl_type == GSL_GL)
+	{
+		gsl_integration_glfixed_table_free(t);
+	}
+
+	else
+	{
+		gsl_integration_workspace_free(w);
+	}
+}
+
+double Integrator::evaluateInftyToInfty()
+{
+	double result = 0, error = 0;
+	
+	if (gsl_type == GSL_GL)
+	{
+		result = gsl_integration_glfixed(&F, -GL_INFINTY, GL_INFINTY, t);
+	}
+
+	else
+	{
+	gsl_integration_qagi(	&F, \
+							ABS_ERROR, REL_ERROR, \
+							WORKSPACE_SIZE, w, \
+							&result, &error);
+	}
+
+	return result;
+}
+
+double Integrator::evaluateAToInfty(double lower_limit)
+{
+	double result = 0, error = 0;
+
+	if (gsl_type == GSL_GL)
+	{
+		result = gsl_integration_glfixed(&F, lower_limit, GL_INFINTY, t);
+	}
+
+	else
+	{
+	gsl_integration_qagiu(	&F, lower_limit, \
+							ABS_ERROR, REL_ERROR, \
+							WORKSPACE_SIZE, w, \
+							&result, &error);
+	}
+
+	return result;
+}
+
+double Integrator::evaluate(double lower_limit, double upper_limit)
+{
+	double result = 0, error = 0;
+
+	if (gsl_type == GSL_QAG)
+	{
+		gsl_integration_qag(	&F, lower_limit, upper_limit, \
+								ABS_ERROR, REL_ERROR, \
+								WORKSPACE_SIZE, GSL_INTEG_GAUSS31, w, \
+								&result, &error);
+	} 
+
+	else if (gsl_type == GSL_QAGS)
+	{
+		gsl_integration_qags(	&F, lower_limit, upper_limit, \
+								ABS_ERROR, REL_ERROR, \
+								WORKSPACE_SIZE, w, \
+								&result, &error);
+	}
+
+	else if (gsl_type == GSL_GL)
+	{
+		result = gsl_integration_glfixed(&F, lower_limit, upper_limit, t);
+	}
+
+	return result;
+}
---
+++ gsltools/integrator.hpp
@@ -0,0 +1,34 @@
+#ifndef INTEGRATOR_H
+#define INTEGRATOR_H
+
+#include <gsl/gsl_integration.h>
+
+enum GSL_TYPE
+{
+	GSL_QAG,
+	GSL_QAGS,
+	GSL_GL
+};
+
+class Integrator
+{
+	GSL_TYPE gsl_type;
+	gsl_function F;
+	gsl_integration_workspace *w;
+	gsl_integration_glfixed_table *t;
+
+public:
+	Integrator( GSL_TYPE gsl_type_key, \
+				double (*integrand_function)(double, void*), void *params);
+	~Integrator();
+	
+	//double setFunctionNParams(double (*integrand_function)(double, void*), void *params);
+	// Intfy functions use a different algorithym 
+	// and may not give desired values of GSL_GL.
+	double evaluateInftyToInfty();
+	double evaluateAToInfty(double lower_limit);
+	
+	double evaluate(double lower_limit, double upper_limit);
+};
+
+#endif
