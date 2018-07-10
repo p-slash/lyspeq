d9fae458513d15a70ff13aef71edfef9d319d6ba
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2018-07-10 11:35:36 -0400

    header after #if

--- io/io_helper_functions.hpp
+++ io/io_helper_functions.hpp
@@ -1,7 +1,7 @@
-#include <complex>
-
 #ifndef IO_HELPER_FUNCTIONS_H
 #define IO_HELPER_FUNCTIONS_H
+
+#include <complex>
 
 void copyArray(const std::complex<double> *source, std::complex<double> *target, long long int size);
 void copyArray(const double *source, double *target, long long int size);
