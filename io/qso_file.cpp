2a60ae3145f1b60bff50f39857c42fc71e178ba8
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2018-07-10 11:36:57 -0400

    Module to read a standard quasar spectra in binary format. The file should start with a header followed by lambda, flux, noise arrays

---
+++ io/qso_file.cpp
@@ -0,0 +1,37 @@
+#include "qso_file.hpp"
+#include "io_helper_functions.hpp"
+
+QSOFile::QSOFile(const char *fname)
+{
+    sprintf(file_name, "%s", fname);
+
+    qso_file = open_file(file_name, 'rb');
+}
+
+QSOFile::~QSOFile()
+{
+    fclose(qso_file);
+}
+
+void QSOFile::readParameters(int &data_number, double &z, double &resolution, double &sig2noi)
+{
+    #define SPEED_OF_LIGHT 299792.458
+
+    rewind(qso_file);
+
+    fread(&header, sizeof(qso_io_header), 1, qso_file);
+
+    data_number = header.data_size;
+    z           = header.redshift;
+    resolution  = SPEED_OF_LIGHT  / (1.0 * header.spectrograph_resolution);
+    sig2noi     = header.signal_to_noise;
+}
+
+void QSOFile::readData(double *lambda, double *flux, double *noise)
+{
+    fseek(qso_file, sizeof(qso_io_header), SEEK_SET);
+
+    fread(lambda, header.data_size * sizeof(double), 1, qso_file);
+    fread(flux,   header.data_size * sizeof(double), 1, qso_file);
+    fread(noise,  header.data_size * sizeof(double), 1, qso_file);
+}
---
+++ io/qso_file.hpp
@@ -0,0 +1,40 @@
+#ifndef QSO_FILE_H
+#define QSO_FILE_H
+
+#include <cstdio>
+
+class QSOFile
+{
+    FILE *qso_file;
+    char file_name[256];
+    // char read_write[3];
+
+    struct qso_io_header
+    {
+        int data_size;
+
+        double redshift;
+        double declination;
+        double right_ascension;
+
+        int spectrograph_resolution;
+        double signal_to_noise;
+
+        double lower_observed_lambda;
+        double upper_observed_lambda;
+
+        double lower_rest_lambda;
+        double upper_rest_lambda;
+    } header;
+
+public:
+    QSOFile(const char *fname);
+    ~QSOFile();
+
+    void readParameters(int &data_number, double &z, double &resolution, double &sig2noi);
+
+    void readData(double *lambda, double *flux, double *noise);
+    
+};
+
+#endif
