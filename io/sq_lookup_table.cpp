8694c29f4b01d197069c3b0dfca25c987c171e8a
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2018-08-28 10:36:15 -0400

    file convention for lookup tables. can read look up table.

--- io/sq_lookup_table.cpp
+++ io/sq_lookup_table.cpp
@@ -1,6 +1,25 @@
 #include "sq_lookup_table.hpp"
 #include "io_helper_functions.hpp"
 
+void QTableFileNameConvention(  char *fname, const char *OUTPUT_DIR, const char *OUTPUT_FILEBASE_Q, \
+                                int r, double k1, double k2, double z)
+{
+    sprintf(fname, "%s/%s_R%d_k%.1e_%.1e_z%.1f.dat", OUTPUT_DIR, OUTPUT_FILEBASE_Q, r, k1, k2, z);
+}
+
+void STableFileNameConvention(  char *fname, const char *OUTPUT_DIR, const char *OUTPUT_FILEBASE_S, \
+                                int r)
+{
+    sprintf(fname, "%s/%s_R%d.dat", OUTPUT_DIR, OUTPUT_FILEBASE_S, r);
+}
+
+// return y = c + deltaY / (N-1) * n
+double getLinearlySpacedValue(double c, double delta_y, int N, int n)
+{
+    return c + delta_y / (N - 1.) * n;
+}
+
+
 SQLookupTable::SQLookupTable(const char *fname, char rw)
 {
     sprintf(file_name, "%s", fname);
@@ -38,6 +57,37 @@ void SQLookupTable::setHeader(int nv, int nz, int R, double dv, double z, double
     isHeaderSet = true;
 }
 
+void SQLookupTable::readHeader()
+{
+    if (!isHeaderSet)
+    {
+        rewind(sq_file);
+        fread(&header, sizeof(sq_io_header), 1, sq_file);
+        
+        isHeaderSet = true;
+    }
+}
+
+void SQLookupTable::readHeader(int &nv, int &nz, int &R, double &dv, double &z, double &ki, double &kf)
+{
+
+    if (read_write[0] == 'w')
+    {
+        printf("WARNING: Reading header while writing SQLookupTable does nothing!\n");
+        return;
+    }
+    
+    readHeader();
+
+    nv = header.vpoints;
+    nz = header.zpoints;
+    R  = header.spectrograph_resolution;
+    dv = header.pixel_width;
+    z  = header.redshift;
+    ki = header.initial_k;
+    kf = header.final_k;
+}
+
 void SQLookupTable::writeData(double *data)
 {
     if (!isHeaderSet)
@@ -54,9 +104,17 @@ void SQLookupTable::writeData(double *data)
     fwrite(data, size, sizeof(double), sq_file);
 }
 
-void readData(double *v_ij, double *z_ij, double *data)
+void SQLookupTable::readData(double *data)
 {
+    readHeader();
+    
+    fseek(sq_file, sizeof(sq_io_header), SEEK_SET);
 
+    printf("Reading SQLookupTable %s.\n", file_name);
+    
+    int size = header.vpoints * header.zpoints;
+
+    fread(data, size, sizeof(double), sq_file);
 }
 
 
--- io/sq_lookup_table.hpp
+++ io/sq_lookup_table.hpp
@@ -3,6 +3,15 @@
 
 #include <cstdio>
 
+void QTableFileNameConvention(  char *fname, const char *OUTPUT_DIR, const char *OUTPUT_FILEBASE_Q, \
+                                int r, double k1, double k2, double z);
+
+void STableFileNameConvention(  char *fname, const char *OUTPUT_DIR, const char *OUTPUT_FILEBASE_S, \
+                                int r);
+
+// Returns y = c + deltaY / (N-1) * n
+double getLinearlySpacedValue(double c, double delta_y, int N, int n);
+
 class SQLookupTable
 {
     FILE *sq_file;
@@ -24,11 +33,15 @@ class SQLookupTable
     
     bool isHeaderSet;
 
+    void readHeader();
+    
 public:
     SQLookupTable(const char *fname, char rw);
     ~SQLookupTable();
 
     void setHeader(int nv, int nz, int R, double dv, double z, double ki, double kf);
+
+    void readHeader(int &nv, int &nz, int &R, double &dv, double &z, double &ki, double &kf);
 
     void readData(double *data);
     void writeData(double *data);
