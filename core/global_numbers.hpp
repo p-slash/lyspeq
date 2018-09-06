516e2b1d4aed8c9ae05277bbbd5d05f02a044d79
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2018-09-06 10:23:20 -0400

    defined numbers in this file

---
+++ core/global_numbers.hpp
@@ -0,0 +1,13 @@
+// Mathematical numbers
+#define PI 3.14159265359
+
+// Physical constants
+#define LYA_REST 1215.67
+#define SPEED_OF_LIGHT 299792.458
+
+// One QSO Estimate numbers
+#define ADDED_CONST_TO_COVARIANCE 10.0
+
+// Quadratic Estimate numbers
+#define CONVERGENCE_EPS 1E-4
+
