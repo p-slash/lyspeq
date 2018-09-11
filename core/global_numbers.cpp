0d0a7975ae8b44b59f6516f4e60466ea9ec9eb0c
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2018-09-11 16:34:10 -0400

    added time variables to keep track of the time cost of each operation

--- core/global_numbers.cpp
+++ core/global_numbers.cpp
@@ -1,6 +1,21 @@
 #include "global_numbers.hpp"
 
-int NUMBER_OF_K_BANDS, NUMBER_OF_Z_BINS;
+#include <cstdio>
+int NUMBER_OF_K_BANDS, NUMBER_OF_Z_BINS, TOTAL_KZ_BINS;
 double *KBAND_EDGES, *KBAND_CENTERS;
 double Z_BIN_WIDTH, *ZBIN_CENTERS;
 
+float   time_spent_on_c_inv    = 0, time_spent_on_f_inv   = 0;
+float   time_spent_on_set_sfid = 0, time_spent_set_qs     = 0, \
+        time_spent_set_modqs   = 0, time_spent_set_fisher = 0;
+
+void printf_time_spent_details()
+{
+    printf("Total time spent on inverting C is %.2f mins.\n", time_spent_on_c_inv / 60.);
+    printf("Total time spent on inverting F is %.2f mins.\n", time_spent_on_f_inv / 60.);
+
+    printf("Total time spent on setting Sfid is %.2f mins.\n", time_spent_on_set_sfid / 60.);
+    printf("Total time spent on setting Qs is %.2f mins.\n", time_spent_set_qs / 60.);
+    printf("Total time spent on setting Mod Qs is %.2f mins.\n", time_spent_set_modqs / 60.);
+    printf("Total time spent on setting F is %.2f mins.\n", time_spent_set_fisher / 60.);
+}
--- core/global_numbers.hpp
+++ core/global_numbers.hpp
@@ -12,6 +12,13 @@
 #define CONVERGENCE_EPS 1E-4
 
 // Binning numbers
-extern int NUMBER_OF_K_BANDS, NUMBER_OF_Z_BINS;
+extern int NUMBER_OF_K_BANDS, NUMBER_OF_Z_BINS, TOTAL_KZ_BINS;
 extern double *KBAND_EDGES, *KBAND_CENTERS;
 extern double Z_BIN_WIDTH, *ZBIN_CENTERS;
+
+// Keeping track of time
+extern float time_spent_on_c_inv, time_spent_on_f_inv;
+extern float time_spent_on_set_sfid, time_spent_set_qs, \
+             time_spent_set_modqs, time_spent_set_fisher;
+
+void printf_time_spent_details();
