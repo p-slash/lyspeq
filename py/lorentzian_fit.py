e687178d6cbae0cd9f5419ba4eb2b4d4cb321628
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2018-10-25 17:29:18 -0400

    fitting 1d with 4 parameters, ignore z component

--- py/lorentzian_fit.py
+++ py/lorentzian_fit.py
@@ -26,6 +26,10 @@ def pd13_lorentzian(X, A, n, alpha, B, beta, lmd):
     k, z = X
     return pd13form_fitting_function(X, A, n, alpha, B, beta)  / (1. + lmd * k**2)
 
+def pd13_lorentzian_noz(k, A, n, alpha, lmd):
+    X = (k, 3.)
+    return pd13form_fitting_function(X, A, n, alpha, 0, 0) / (1. + lmd * k**2)
+
 input_ps = sys.argv[1]
 output_ps= sys.argv[2]
 
@@ -39,49 +43,47 @@ NkBins = size[1]
 
 z, k, p, e = np.genfromtxt(input_ps, delimiter = ' ', skip_header = 2, unpack = True)
 
-p = p * k / np.pi
-e = e * k / np.pi
+p = p 
+e = e 
 
 theoretical_ps = np.zeros(NzBins * NkBins)
 
 # Fit using the same pd13 form, get new params
-pd13_0 = 0.0662, -2.685, -0.223, 3.591, -0.177, 359.8
+pd13_0 = 0.0662, -2.685, -0.223, 359.8
 
 for nz in range(NzBins):
     ind_1 = nz * NkBins
     ind_2 = (nz + 1) * NkBins
 
-    z1 = z[ind_1:ind_2]
     k1 = k[ind_1:ind_2]
     p1 = p[ind_1:ind_2]
     e1 = e[ind_1:ind_2]
 
     mask = np.greater(p1, 0)
     
-    z1_m = z1[mask]
     k1_m = k1[mask]
     p1_m = p1[mask]
     e1_m = e1[mask]
 
-    pnew, pcov = curve_fit(pd13_lorentzian, (k1_m,z1_m), p1_m, pd13_0, sigma=e1_m)
+    pnew, pcov = curve_fit(pd13_lorentzian_noz, k1_m, p1_m, pd13_0, sigma=e1_m)
 
-    theoretical_ps[ind_1:ind_2] = pd13_lorentzian((k1,z1), *pnew)
+    theoretical_ps[ind_1:ind_2] = pd13_lorentzian_noz(k1, *pnew)
+
+#     r                = p1 - theoretical_ps[ind_1:ind_2]
+#     chisq            = np.sum((r/e1)**2)
+#     df               = len(p1) - 4
+
+#     fit_param_text = """A        = %.3e
+# n        = %.3e
+# alpha    = %.3e
+# lambda   = %.3e""" % (pnew[0], pnew[1], pnew[2], pnew[3])
+#     print(fit_param_text)
+#     print("chisq = ", chisq, "doff = ",df, "chisq/dof = ", chisq/df)
 
 np.savetxt(output_ps, theoretical_ps)
     
 exit(0)
     
-    # r                = p1 - theoretical_ps
-    # chisq            = np.sum((r/error)**2)
-    # df               = len(power_sp) - 6
-
-    # fit_param_text = """A        = %.3e
-    # n        = %.3e
-    # alpha    = %.3e
-    # B        = %.3e
-    # beta     = %.3e
-    # lambda   = %.3e""" % (pnew[0], pnew[1], pnew[2], pnew[3], pnew[4], pnew[5])
-    # print(fit_param_text)
-    # print("chisq = ", chisq, "doff = ",df, "chisq/dof = ", chisq/df)
+    
 
 
