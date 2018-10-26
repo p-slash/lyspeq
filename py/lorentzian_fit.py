9ae01b6c0954dcb2aebfc9a058aa7fdc290c34c4
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2018-10-26 15:13:58 -0400

    fit all redshifts, instead of fitting each z bin individually

--- py/lorentzian_fit.py
+++ py/lorentzian_fit.py
@@ -18,9 +18,9 @@ def pd13form_fitting_function(X, A, n, alpha, B, beta):
     lnkP_pi = np.log(A) \
             + (3. + n) * lnk \
             + alpha * lnk * lnk \
-            + (B + beta * lnk) * lnz;
+            + (B + beta * lnk) * lnz
 
-    return np.exp(lnkP_pi) * np.pi / k;
+    return np.exp(lnkP_pi) * np.pi / k
 
 def pd13_lorentzian(X, A, n, alpha, B, beta, lmd):
     k, z = X
@@ -28,58 +28,72 @@ def pd13_lorentzian(X, A, n, alpha, B, beta, lmd):
 
 def pd13_lorentzian_noz(k, A, n, alpha, lmd):
     X = (k, 3.)
-    return pd13form_fitting_function(X, A, n, alpha, 0, 0) / (1. + lmd * k**2)
+    return pd13_lorentzian(X, A, n, alpha, 0, 0, lmd)
 
 input_ps = sys.argv[1]
 output_ps= sys.argv[2]
 
-f = open(input_ps, 'r')
-string_sizes = f.readline()
-size = [int(n) for n in string_sizes.split()]
-f.close()
+# f = open(input_ps, 'r')
+# string_sizes = f.readline()
+# size = [int(n) for n in string_sizes.split()]
+# f.close()
 
-NzBins = size[0]
-NkBins = size[1]
+# NzBins = size[0]
+# NkBins = size[1]
 
 z, k, p, e = np.genfromtxt(input_ps, delimiter = ' ', skip_header = 2, unpack = True)
 
-theoretical_ps = np.zeros(NzBins * NkBins)
+theoretical_ps = np.zeros(len(z))
 
-# Fit using the same pd13 form, get new params
-pd13_0 = 0.0662, -2.685, -0.223, 359.8
+# Global Fit
+# Fit using the same fiducial parameters
+fiducial_params = 0.0662, -2.685, -0.223, 3.591, -0.177, 360.
 
-for nz in range(NzBins):
-    ind_1 = nz * NkBins
-    ind_2 = (nz + 1) * NkBins
+mask     = np.greater(p, 0.)
+z_masked = z[mask]
+k_masked = k[mask]
+p_masked = p[mask]
+e_masked = e[mask]
 
-    k1 = k[ind_1:ind_2]
-    p1 = p[ind_1:ind_2]
-    e1 = e[ind_1:ind_2]
+pnew, pcov     = curve_fit(pd13_lorentzian, (k_masked, z_masked), p_masked, fiducial_params, sigma=e_masked)
+theoretical_ps = pd13_lorentzian((k, z), *pnew)
 
-    mask = np.greater(p1, 0)
-    
-    k1_m = k1[mask]
-    p1_m = p1[mask]
-    e1_m = e1[mask]
+r              = p_masked - theoretical_ps[mask]
+chisq          = np.sum((r/e_masked)**2)
+df             = len(p_masked) - 6
 
-    pnew, pcov = curve_fit(pd13_lorentzian_noz, k1_m, p1_m, pd13_0, sigma=e1_m)
-
-    theoretical_ps[ind_1:ind_2] = pd13_lorentzian_noz(k1, *pnew)
-
-#     r                = p1 - theoretical_ps[ind_1:ind_2]
-#     chisq            = np.sum((r/e1)**2)
-#     df               = len(p1) - 4
-
-#     fit_param_text = """A        = %.3e
-# n        = %.3e
-# alpha    = %.3e
-# lambda   = %.3e""" % (pnew[0], pnew[1], pnew[2], pnew[3])
-#     print(fit_param_text)
-#     print("chisq = ", chisq, "doff = ",df, "chisq/dof = ", chisq/df)
+fit_param_text = """A        = %.3e
+n        = %e
+alpha    = %e
+B        = %e
+beta     = %e
+lambda   = %e""" % (pnew[0], pnew[1], pnew[2], pnew[3], pnew[4], pnew[5])
+print(fit_param_text)
+print("chisq = ", chisq, "doff = ",df, "chisq/dof = ", chisq/df)
 
 np.savetxt(output_ps, theoretical_ps)
     
 exit(0)
+
+# for nz in range(NzBins):
+#     ind_1 = nz * NkBins
+#     ind_2 = (nz + 1) * NkBins
+
+#     k1 = k[ind_1:ind_2]
+#     p1 = p[ind_1:ind_2]
+#     e1 = e[ind_1:ind_2]
+
+#     mask = np.greater(p1, 0)
+    
+#     k1_m = k1[mask]
+#     p1_m = p1[mask]
+#     e1_m = e1[mask]
+
+#     pnew, pcov = curve_fit(pd13_lorentzian_noz, k1_m, p1_m, pd13_0, sigma=e1_m)
+
+#     theoretical_ps[ind_1:ind_2] = pd13_lorentzian_noz(k1, *pnew)
+
+
     
     
 
