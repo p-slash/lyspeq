394c939ef2438c44f4d1d32042d405a9a50ae9da
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2018-11-09 14:21:12 -0500

    fix all

--- py/lorentzian_fit.py
+++ py/lorentzian_fit.py
@@ -29,88 +29,109 @@ def pd13_lorentzian_fitting_function_z(X, A, n, alpha, B, beta, lmd):
     
     p_z_mult = np.exp((B + beta * lnk) * lnz)
 
-    return pd13_fitting_function_k(k, A, n, alpha, lmd) * p_z_mult
+    return pd13_lorentzian_fitting_function_k(k, A, n, alpha, lmd) * p_z_mult
 
-# def jacobian_k(k, A, n, alpha):
-#     jac_k_res = np.zeros(3)
+def jacobian_k(k, A, n, alpha, lmd):
+    lnk       = np.log(k / K_0)
+    p_k       = pd13_lorentzian_fitting_function_k(k, A, n, alpha, lmd)
 
-input_ps  = sys.argv[1]
-output_ps = sys.argv[2]
+    return np.column_stack((p_k / A, p_k * lnk, p_k * lnk * lnk, -p_k * k**2 / (1. + lmd * k**2))) 
 
-if len(sys.argv) == 9:
-    Z_EVO_ON        = True
-    NUMBER_OF_PARAMS= 6
-    fiducial_params = np.array(sys.argv[3:], dtype=float)
-elif len(sys.argv) == 7:
-    Z_EVO_ON        = False
-    NUMBER_OF_PARAMS= 4
-    fiducial_params = np.array(sys.argv[3:], dtype=float)
-else:
-    print("Not enough arguments. Pass fiducial cosmology parameters.")
-    exit(1)
+def jacobian_z(X, A, n, alpha, B, beta, lmd):
+    k, z = X
 
-z, k, p, e = np.genfromtxt(input_ps, delimiter = ' ', skip_header = 2, unpack = True)
+    lnk = np.log(k / K_0)
+    lnz = np.log((1. + z) / (1. + Z_0))
 
-theoretical_ps = np.zeros(len(z))
+    p_z = pd13_lorentzian_fitting_function_z(X, A, n, alpha, B, beta, lmd)
 
-# Global Fit
-mask     = np.logical_and(np.greater(p, 0.), np.greater(e, 0.))
-z_masked = z[mask]
-k_masked = k[mask]
-p_masked = p[mask]
-e_masked = e[mask]
+    return np.column_stack((p_z / A  , p_z * lnk      ,  p_z * lnk * lnk, \
+                            p_z * lnz, p_z * lnk * lnz, -p_z * k**2 / (1. + lmd * k**2))) 
 
-if Z_EVO_ON:
-    fit_function = pd13_lorentzian_fitting_function_z
-    X_masked     = (k_masked, z_masked)
-    X            = (k, z)
-else:
-    fit_function = pd13_lorentzian_fitting_function_k
-    X_masked     = k_masked
-    X            = k
-        
-try:
-    lb = [0, np.full(NUMBER_OF_PARAMS-2, -np.inf), 0]
-    pnew, pcov = curve_fit(fit_function, X_masked, p_masked, fiducial_params, \
-                            sigma=e_masked, bounds=(lb, np.inf))
-except ValueError:
-    print("ValueError: Either ydata or xdata contain NaNs.")
-    exit(1)
-except RuntimeError:
-    print("RuntimeError: The least-squares minimization fails.")
-    exit(1)
-except OptimizeWarning:
-    print("OptimizeWarning: Covariance of the parameters can not be estimated. Using fiducial parameters instead.")    
-    pnew = fiducial_params
+if __name__ == '__main__':    
+    input_ps  = sys.argv[1]
+    output_ps = sys.argv[2]
 
-theoretical_ps = fit_function(X, *pnew)
+    if len(sys.argv) == 9:
+        Z_EVO_ON        = True
+        NUMBER_OF_PARAMS= 6
+        fiducial_params = np.array(sys.argv[3:], dtype=float)
+    elif len(sys.argv) == 7:
+        Z_EVO_ON        = False
+        NUMBER_OF_PARAMS= 4
+        fiducial_params = np.array(sys.argv[3:], dtype=float)
+    else:
+        print("Not enough arguments. Pass fiducial cosmology parameters.")
+        exit(1)
 
-r              = p_masked - theoretical_ps[mask]
-chisq          = np.sum((r/e_masked)**2)
-df             = len(p_masked) - NUMBER_OF_PARAMS
+    z, k, p, e = np.genfromtxt(input_ps, delimiter = ' ', skip_header = 2, unpack = True)
 
-pnew_toprint = np.zeros(6)
-if Z_EVO_ON:
-    pnew_toprint = pnew
-else:
-    pnew_toprint[0:3] = pnew[0:3]
-    pnew_toprint[5]   = pnew[3]
+    theoretical_ps = np.zeros(len(z))
 
-fit_param_text = """A        = %.3e
+    # Global Fit
+    mask     = np.logical_and(np.greater(p, 0.), np.greater(e, 0.))
+    z_masked = z[mask]
+    k_masked = k[mask]
+    p_masked = p[mask]
+    e_masked = e[mask]
+
+    if Z_EVO_ON:
+        fit_function = pd13_lorentzian_fitting_function_z
+        jac_function = jacobian_z
+        X_masked     = (k_masked, z_masked)
+        X            = (k, z)
+    else:
+        fit_function = pd13_lorentzian_fitting_function_k
+        jac_function = jacobian_k
+        X_masked     = k_masked
+        X            = k
+            
+    try:
+        lb = np.full(NUMBER_OF_PARAMS, -np.inf)
+        lb[0]  = 0
+        lb[-1] = 0
+        print(lb)
+        pnew, pcov = curve_fit(fit_function, X_masked, p_masked, fiducial_params, \
+                                sigma=e_masked, absolute_sigma=True, bounds=(lb, np.inf), \
+                                method='trf', jac=jac_function)
+    except ValueError:
+        raise
+        exit(1)
+    except RuntimeError:
+        raise
+        exit(1)
+    except OptimizeWarning:
+        print("OptimizeWarning: Covariance of the parameters can not be estimated. Using fiducial parameters instead.")    
+        pnew = fiducial_params
+
+    theoretical_ps = fit_function(X, *pnew)
+
+    r              = p_masked - theoretical_ps[mask]
+    chisq          = np.sum((r/e_masked)**2)
+    df             = len(p_masked) - NUMBER_OF_PARAMS
+
+    pnew_toprint = np.zeros(6)
+    if Z_EVO_ON:
+        pnew_toprint = pnew
+    else:
+        pnew_toprint[0:3] = pnew[0:3]
+        pnew_toprint[5]   = pnew[3]
+
+    fit_param_text = """A        = %.3e
 n        = %e
 alpha    = %e
 B        = %e
 beta     = %e
 lambda   = %e""" % (pnew_toprint[0], pnew_toprint[1], pnew_toprint[2], \
-                    pnew_toprint[3], pnew_toprint[4], pnew_toprint[5])
-print(fit_param_text)
-print("chisq = ", chisq, "doff = ",df, "chisq/dof = ", chisq/df)
+                        pnew_toprint[3], pnew_toprint[4], pnew_toprint[5])
+    print(fit_param_text)
+    print("chisq = ", chisq, "doff = ",df, "chisq/dof = ", chisq/df)
 
-params_header = "%e %e %e %e %e %e" % ( pnew_toprint[0], pnew_toprint[1], pnew_toprint[2], \
-                                        pnew_toprint[3], pnew_toprint[4], pnew_toprint[5])
-np.savetxt(output_ps, theoretical_ps, header=params_header, comments='')
-    
-exit(0)
+    params_header = "%e %e %e %e %e %e" % ( pnew_toprint[0], pnew_toprint[1], pnew_toprint[2], \
+                                            pnew_toprint[3], pnew_toprint[4], pnew_toprint[5])
+    np.savetxt(output_ps, theoretical_ps, header=params_header, comments='')
+        
+    exit(0)
 
 # for nz in range(NzBins):
 #     ind_1 = nz * NkBins
