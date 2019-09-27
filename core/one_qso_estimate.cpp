0ab0114b611d08870018b8433ed739a2f14494ed
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2019-09-27 12:30:46 -0400

    disable continuum derivative marginalization

--- core/one_qso_estimate.cpp
+++ core/one_qso_estimate.cpp
@@ -320,18 +320,19 @@ void OneQSOEstimate::setCovarianceMatrix(const double *ps_estimate)
     gsl_matrix_add_constant(covariance_matrix, ADDED_CONST_TO_COVARIANCE);
 
     // Continuum derivative
+    #if 0
     double *temp_t_vector = new double[DATA_SIZE];
     double MEDIAN_LAMBDA = LYA_REST * (1 + MEDIAN_REDSHIFT);
 
     for (int tvec_i = 0; tvec_i < DATA_SIZE; ++tvec_i)
         temp_t_vector[tvec_i] = log(lambda_array[tvec_i]/MEDIAN_LAMBDA);
 
-    // cblas_dspr(CblasRowMajor, CblasUpper, DATA_SIZE, ADDED_CONST_TO_COVARIANCE, temp_t_vector, 1, covariance_matrix->data);
     cblas_dger(CblasRowMajor, DATA_SIZE, DATA_SIZE, ADDED_CONST_TO_COVARIANCE, 
         temp_t_vector, 1, temp_t_vector, 1, covariance_matrix->data, DATA_SIZE);
 
-    // mxhelp::fprintfMatrix("cov.txt", covariance_matrix);
     delete [] temp_t_vector;
+    #endif
+    
     #undef ADDED_CONST_TO_COVARIANCE
 
     isCovInverted = false;
