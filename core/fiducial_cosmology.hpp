4a7d391e70a54e506699204a92da1ee96c9e1163
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2019-10-25 13:43:35 -0400

    fiducialPowerSpectrum converted to a function pointer. FIDUCIAL_PD13_PARAMS has default value. sinc and spectral_response_window_fn are inline.

--- core/fiducial_cosmology.hpp
+++ core/fiducial_cosmology.hpp
@@ -19,6 +19,20 @@ namespace conv
 
 namespace fidcosmo
 {
+    // Default fiducial power is Palanque Delabrouille fit.
+    // Calling setFiducialPowerFromFile changes this pointer to interpolationFiducialPower
+    extern double (*fiducialPowerSpectrum)(double k, double z, void *params);
+    // Currently growth factor is always given Palanqu Delabrouille fit.
+    extern double (*fiducialPowerGrowthFactor)(double z_ij, double k_kn, double z_zm, void *params);
+    extern bool USE_INTERP_FIDUCIAL_POWER;
+
+    // Assume file starts with two integers, then has three columns
+    // Nk Nz
+    // z k P
+    // . . .
+    // Power is ordered for each redshift bin
+    void setFiducialPowerFromFile(const char *fname);
+
     namespace pd13
     {
         // Fitting function has the form in Palanque-Delabrouille et al. 2013
@@ -34,17 +48,15 @@ namespace fidcosmo
 
             double lambda;
         } pd13_fit_params;
-
+    
+        // Default parameters are fit parameters if not specified in config file.
         extern pd13_fit_params FIDUCIAL_PD13_PARAMS;
     }
-
-    // This function is defined in preprocessing
-    double fiducialPowerSpectrum(double k, double z, void *params);
-    double fiducialPowerGrowthFactor(double z_ij, double k_kn, double z_zm, void *params);
 }
 
 namespace fidpd13 = fidcosmo::pd13;
 
+// Data structures to integrate signal and derivative matrix expressions
 struct spectrograph_windowfn_params
 {
     double delta_v_ij;
@@ -53,9 +65,6 @@ struct spectrograph_windowfn_params
     double spectrograph_res;    // This is 1 sigma km/s resolution
 };
 
-double spectral_response_window_fn(double k, struct spectrograph_windowfn_params *spec_params);
-
-// Data structures to integrate signal and derivative matrix expressions
 struct sq_integrand_params
 {
     fidpd13::pd13_fit_params            *fiducial_pd_params;
@@ -63,7 +72,6 @@ struct sq_integrand_params
 };
 
 double q_matrix_integrand(double k, void *params);
-
 double signal_matrix_integrand(double k, void *params);
 
 #endif
