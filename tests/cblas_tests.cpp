770d892653785a12064c8815b13b38ed0ae06698
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2020-02-07 10:33:33 -0500

    tests on cblas_dsymv, trace_ddiagmv and LU invert

--- tests/cblas_tests.cpp
+++ tests/cblas_tests.cpp
@@ -3,8 +3,11 @@
 #include <cmath>
 
 #define N 8
+#define NA 4
+
 int main()
 {
+    // Test cblas_ddot
     double A[N], B[N], C=0, r=-10, rel_err = 0;
     for (int i = 0; i < N; ++i)
     {
@@ -18,10 +21,42 @@ int main()
 
     rel_err = fabs(C-r)/C;
     if (rel_err > 1e-8)
-    {
-        fprintf(stderr, "True and CBLAS does not match. Rel error: %e.\n", rel_err);
-        return 1;
-    }
+        fprintf(stderr, "ERROR cblas_ddot: True and CBLAS does not match. Rel error: %e.\n", rel_err);
+
+    // Test cblas_dsymv
+    double smy_matrix_A[] = { 4, 6, 7, 8,
+                            6, 9, 2, 1,
+                            7, 2, 0, 1,
+                            8, 1, 1, 5};
+    gsl_matrix_view gmv_symA = gsl_matrix_view_array(smy_matrix_A, NA, NA);
+
+    double vector_B[] = {4, 5, 6, 7}, vector_R[NA];
+    gsl_vector_view gvv_B = gsl_vector_view_array(vector_B, NA);
+
+    cblas_dsymv(CblasRowMajor, CblasUpper,
+                NA, 0.5, smy_matrix_A, NA,
+                vector_B, 1,
+                0, vector_R, 1);
+
+    for (int i = 0; i < NA; ++i)
+        printf("%lf ", vector_R[i]);
+
+    printf("\n");
+
+    // Test trace_ddiagmv
+    printf("%lf\n", mxhelp::trace_ddiagmv(&gmv_symA.matrix, vector_B));
+
+    // my_cblas_dsymvdot
+    printf("%lf\n", mxhelp::my_cblas_dsymvdot(&gvv_B.vector, &gmv_symA.matrix));
+    
+    // Test LU invert
+    gsl_matrix *copy_A = gsl_matrix_alloc(NA, NA);
+    gsl_matrix_memcpy(copy_A, &gmv_symA.matrix);
+    
+    mxhelp::invertMatrixLU(copy_A, &gmv_symA.matrix);
+    mxhelp::printfMatrix(&gmv_symA.matrix);
+
+    gsl_matrix_free(copy_A);
 
     return 0;
 }
