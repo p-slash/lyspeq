9993b8b2d582d92c4fabd6c41b5a69e73626761a
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2022-05-24 12:19:26 -0400

    move print

--- core/chunk_estimate.cpp
+++ core/chunk_estimate.cpp
@@ -190,6 +190,9 @@ bool Chunk::_isAboveNyquist(int i_kz)
 
 Chunk::Chunk(Chunk &&rhs)
 {
+    #ifdef DEBUG
+    LOG::LOGGER.ERR("moving %s\n", qmaster.fname.c_str());
+    #endif
     qFile = std::move(rhs.qFile);
     rhs.qFile = NULL;
 
