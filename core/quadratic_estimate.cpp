71388a0260e696bc81efbcc0bd2e2a303fc39ec6
Naim Goksel Karacayli <ngokselk@gmail.com>
2023-06-19 14:01:13 -0400

    read ra & dec from qso file and save them in bootchunks

--- core/quadratic_estimate.cpp
+++ core/quadratic_estimate.cpp
@@ -49,7 +49,8 @@ void _saveChunkResults(
                 pk, nk, tk,
                 one_chunk->fisher_matrix.get(), ndim,
                 one_chunk->fisher_index_start,
-                one_chunk->qFile->id, one_chunk->qFile->z_qso);
+                one_chunk->qFile->id, one_chunk->qFile->z_qso,
+                one_chunk->qFile->ra, one_chunk->qFile->dec);
         }
     }
 }
