f6a9e11c6c6b2d392535a0b2fc1bde40e54d2c65
Naim Goksel Karacayli <ngokselk@gmail.com>
2024-03-30 14:33:56 -0400

    delete move and copy from Exposure

--- cross/exposure.hpp
+++ cross/exposure.hpp
@@ -23,11 +23,10 @@ public:
         _setNQandFisherIndex();
         glmemory::setMaxSizes(size(), size(), 2 * N_Q_MATRICES, false);
         N_Q_MATRICES = 0;
-    };
-    ~Exposure() {
-        if (local_cov_mat || weighted_data)
-            deallocMatrices();
     }
+    Exposure(Exposure &&rhs) = delete;
+    Exposure(const Exposure &rhs) = delete;
+    ~Exposure() { if (local_cov_mat || weighted_data) deallocMatrices(); }
 
     int getExpId() const { return qFile->expid; };
     int getNight() const { return qFile->night; };
