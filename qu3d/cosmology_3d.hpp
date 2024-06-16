5418ae97a8d16042c444054662a2979e27a1c321
Naim Goksel Karacayli <ngokselk@gmail.com>
2024-06-15 20:23:23 -0400

    cosmology class

---
+++ qu3d/cosmology_3d.hpp
@@ -0,0 +1,90 @@
+#ifndef COSMOLOGY_3D_H
+#define COSMOLOGY_3D_H
+
+#include <cmath>
+#include <memory>
+#include <string>
+
+#include "core/global_numbers.hpp"
+#include "mathtools/discrete_interpolation.hpp"
+#include "io/config_file.hpp"
+
+
+namespace conv {
+    void convertLambdaToOnePlusRedshift(double *lambda, int size) {
+        std::for_each(lambda, lambda + size, [](double &ld) {
+            ld = ld / LYA_REST;
+        });
+    }
+}
+
+namespace fidcosmo {
+    const config_map planck18_default_parameters ({
+        {"OmegaMatter", "0.30966"}, {"OmegaRadiation", "5.402015137139352e-05"},
+        {"Hubble", "67.66"}
+    });
+
+    class FlatLCDM {
+        double Omega_m, Omega_r, H0, Omega_L;
+        std::unique_ptr<DiscreteInterpolation1D>
+            interp_comov_dist, hubble_z;
+    public:
+        /* This function reads following keys from config file:
+        OmegaMatter: double
+        OmegaRadiation: double
+        Hubble: double
+        */
+        FlatLCDM(ConfigFile &config) {
+            config.addDefaults(planck18_default_parameters);
+            Omega_m = config.getDouble("OmegaMatter");
+            Omega_r = config.getDouble("OmegaRadiation");
+            H0 = config.getDouble("Hubble");
+            Omega_L = 1 - Omega_m - Omega_r * (1 + _nu_relative_density(1))
+
+            // Cache
+            const int nz = 100;
+            const double dz = 0.005;
+            double z1arr[nz], Hz[nz], cDist[nz];
+            for (int i = 0; i < nz; ++i) {
+                z1arr[i] = 2.8 + dz * i;
+                Hz[i] = _calcHubble(z1arr[i]);
+            }
+
+            hubble_z = std::make_unique<DiscreteInterpolation1D>(
+                z1arr[0], dz, nz, &Hz[0]);
+        }
+    
+        /* Fitted to astropy Planck18.nu_relative_density function based on
+           Komatsu et al. 2011, eq 26
+        */
+        double _nu_relative_density(
+                z1, A=0.3173, nu_y0=357.91212097, p=1.83, invp=0.54644808743,
+                nmassless=2, B=0.23058962986246165
+        ) {
+            return B * (nmassless + pow(1 + pow(A * nu_y0 / z1, p), invp));
+        }
+
+        /* in km/s/Mpc */
+        double _calcHubble(z1) {
+            double z3 = z1 * z1 * z1;
+            double z4 = z3 * z1;
+            double nu = _nu_relative_density(z1);
+            return H0 * sqrt(Omega_L + Omega_m * z3 + Omega_r * (1 + nu) * z4);
+        }
+
+        double _calcComovingDist(z1) {
+
+        }
+
+        /* in km/s/Mpc */
+        double getHubble(z1) {
+            hubble_z->evaluate(z1);
+        }
+
+        double getComovingDist(z1) {
+            interp_comov_dist->evaluate(z1);
+        }
+    }
+}
+
+#endif
