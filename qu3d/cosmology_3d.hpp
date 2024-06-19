24ac2476e6e7b160dee21b49e1674c9ec74fe1c2
Naim Goksel Karacayli <ngokselk@gmail.com>
2024-06-19 10:20:51 -0400

    change trapz from static to inline

--- qu3d/cosmology_3d.hpp
+++ qu3d/cosmology_3d.hpp
@@ -11,7 +11,7 @@
 #include "io/io_helper_functions.hpp"
 
 
-static double trapz(const double *y, int N, double dx=1.0) {
+inline double trapz(const double *y, int N, double dx=1.0) {
     double result = y[N - 1] / 2;
     for (int i = N - 2; i > 0; --i)
         result += y[i];
