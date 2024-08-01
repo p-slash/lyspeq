b27d9060bbe00f7d3711536792d739cba1c1a7dd
Naim Goksel Karacayli <ngokselk@gmail.com>
2024-08-01 15:51:27 -0700

    fix los fftlog multiplication by sqrt(kz). bias perp transformation with -0.25 too

--- qu3d/ps2cf_2d.hpp
+++ qu3d/ps2cf_2d.hpp
@@ -15,11 +15,20 @@ public:
     Ps2Cf_2D(int nk, double k1, double k2, int smooth_sigma=1) : N(nk) {
         fht_z = std::make_unique<FFTLog>(N);
         fht_xy = std::make_unique<FFTLog>(N);
+
+        sqrt_kz = std::make_unique<double[]>(N);
+        isqrt_rz = std::make_unique<double[]>(N);
+
         if (smooth_sigma > 0)
             smoother = std::make_unique<Smoother>(smooth_sigma);
 
         fht_z->construct(-0.5, k1, k2, -0.25, 0);
-        fht_xy->construct(0, k1, k2, 0, 0);
+        fht_xy->construct(0, k1, k2, -0.25, 0);
+
+        for (int i = 0; i < N; ++i) {
+            sqrt_kz[i] = sqrt(fht_z->r[i]);
+            isqrt_rz[i] = 1.0 / sqrt(fht_z->k[i]);
+        }
     }
 
     const double* getKperp() const { return fht_xy->r.get(); }
@@ -32,7 +41,8 @@ public:
        Returns: xi_SS interpolator in rz, rperp
     */
     std::unique_ptr<DiscreteInterpolation2D> transform(
-            const double *p2d, int truncate, double rmax
+            const double *p2d, int truncate, double rmax,
+            bool return_log_interp=false
     ) {
         int Nres = N - 2 * truncate;
         /* Intermediate array will be transposed */
@@ -67,6 +77,12 @@ public:
         if (smoother)
             smoother->smooth1D(result.get(), Nres, Nres, true);
 
+        if (return_log_interp)
+            return std::make_unique<DiscreteInterpolation2D>(
+                log(fht_z->k[truncate]), fht_z->getDLn(),
+                log(fht_xy->k[truncate]), fht_xy->getDLn(),
+                result.get(), Nres, Nres);
+
         // Evaluting log for all coordinates is too expensive.
         // Convert input rs to log r
         for (int i = 0; i < N; ++i) {
@@ -96,16 +112,19 @@ public:
 
 private:
     int N;
-    std::unique_ptr<double[]> kperp, rperp, interm, result;
+    std::unique_ptr<double[]> sqrt_kz, isqrt_rz, interm, result;
     std::unique_ptr<FFTLog> fht_z, fht_xy;
     std::unique_ptr<Smoother> smoother;
 
     void _fhtZ(const double *pkz, int i) {
         std::copy_n(pkz, N, fht_z->field);
+        for (int j = 0; j < N; ++j)
+            fht_z->field[j] = pkz[j] * sqrt_kz[j];
+
         fht_z->transform();
 
         for (int j = 0; j < N; ++j)
-            interm[i + j * N] = fht_z->field[j] / sqrt(fht_z->k[j]);
+            interm[i + j * N] = fht_z->field[j] * isqrt_rz[j];
     }
 };
 
