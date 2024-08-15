b141945b94569fa64f0b479b7ccbe77c96d72827
Naim Goksel Karacayli <ngokselk@gmail.com>
2024-08-15 08:57:11 -0400

    random: add normal randoms to vector

--- mathtools/my_random.hpp
+++ mathtools/my_random.hpp
@@ -15,6 +15,11 @@ public:
             v[i] = n_dist(rng_engine);
     }
 
+    void addVectorNormal(double *v, unsigned int size) {
+        for (unsigned int i = 0; i < size; ++i)
+            v[i] += n_dist(rng_engine);
+    }
+
     void fillVectorOnes(double *v, size_t size) {
         for (size_t i = 0; i < size; ++i)
             v[i] = (uidist(rng_engine) == 0) ? -1 : +1;
