554e4577065613a02a611b223797337b15afa195
Naim Goksel Karacayli <ngokselk@gmail.com>
2025-02-05 14:34:04 -0800

    minor pade performance improvements through pointer swapping (could be buggy)

--- qu3d/optimal_qu3d_pade.cpp
+++ qu3d/optimal_qu3d_pade.cpp
@@ -1,6 +1,5 @@
-std::vector<double> _pade_xi(int pade_order) {
-    std::vector<double> result;
-    result.resize(pade_order);
+inline std::unique_ptr<double[]> _pade_xi(int pade_order) {
+    auto result = std::make_unique<double[]>(pade_order);
     for (int i = 0; i < pade_order; ++i)
         result[i] = 0.5 * (1.0 + cos((2 * i + 1) * MY_PI / (2 * pade_order)));
     return result;
@@ -16,11 +15,15 @@ void Qu3DEstimator::multiplyCovSmallSqrtPade(int pade_order) {
     // double s = (min_eval + max_eval) / 2.0;
     // double s = 1.0;
 
-    auto xi = _pade_xi(pade_order);
-    std::vector<double> alphas;
-    alphas.resize(xi.size());
-    for (int i = 0; i < pade_order; ++i)
-        alphas[i] = 1.0 / xi[i] - 1.0;
+    auto xi = std::make_unique<double[]>(pade_order),
+         alphas = std::make_unique<double[]>(pade_order);
+
+    for (int i = 0; i < pade_order; ++i) {
+        xi[i] = 0.5 * (1.0 + cos((2 * i + 1) * MY_PI / (2 * pade_order)));
+        xi[i] = 1.0 / xi[i];
+        alphas[i] = xi[i] - 1.0;
+        xi[i] /= pade_order;
+    }
 
     #pragma omp parallel for schedule(dynamic, 4)
     for (auto &qso : quasars)
@@ -30,22 +33,25 @@ void Qu3DEstimator::multiplyCovSmallSqrtPade(int pade_order) {
     if (verbose)
         LOG::LOGGER.STD("  Entered multiplyCovSmallSqrtPade with order %d. "
                         "New tolerance %.2e\n", pade_order, tolerance);
+
     for (int i = 0; i < pade_order; ++i) {
         conjugateGradientIpH(alphas[i]);
+
         #pragma omp parallel for schedule(dynamic, 4)
         for (auto &qso : quasars)
-            cblas_daxpy(qso->N, 1.0 / xi[i], qso->in, 1, qso->sc_eta, 1);
+            cblas_daxpy(qso->N, xi[i], qso->in, 1, qso->sc_eta, 1);
     }
     tolerance /= 10;
 
-    #pragma omp parallel for schedule(dynamic, 4)
+    #pragma omp parallel for
     for (auto &qso : quasars)
-        std::copy_n(qso->sc_eta, qso->N, qso->in);
+        std::swap(qso->sc_eta, qso->in);
 
     multiplyAsVector();
-    // s = sqrt(s);
-    #pragma omp parallel for schedule(dynamic, 4)
-    for (auto &qso : quasars)
-        for (int i = 0; i < qso->N; ++i)
-            qso->truth[i] = qso->out[i] / pade_order;
+
+    #pragma omp parallel for
+    for (auto &qso : quasars) {
+        std::swap(qso->sc_eta, qso->in);
+        std::swap(qso->truth, qso->out);
+    }
 }
