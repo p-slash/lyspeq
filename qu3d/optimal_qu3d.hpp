357331954594ff5d6ff716a2e505b9253682f9ff
Naim Goksel Karacayli <ngokselk@gmail.com>
2026-02-12 11:24:25 -0500

    Fix formatting issue in qu3d_default_parameters for MinBoxLength entry

--- qu3d/optimal_qu3d.hpp
+++ qu3d/optimal_qu3d.hpp
@@ -17,7 +17,7 @@ const config_map qu3d_default_parameters ({
     {"TurnOnPpCovariance", "-1"}, {"NumberOfMultipoles", "4"},
     {"MaxConjGradSteps", "5"}, {"MaxMonteCarlos", "100"},
     {"MinimumRa", "0.0"}, {"MaximumRa", "360.0"},
-    {"MinimumDec", "-90.0"}, {"MaximumDec", "90.0"}, {"MinBoxLength", "0"}
+    {"MinimumDec", "-90.0"}, {"MaximumDec", "90.0"}, {"MinBoxLength", "0"},
     {"MinimumKperp", "0"}, {"MinimumKlos", "0"},
     {"ConvergenceTolerance", "1e-6"}, {"AbsoluteTolerance", "-1"},
     {"LongScale", "50"}, {"ScaleFactor", "4"},
