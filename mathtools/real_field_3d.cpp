28d7cdfdff080d2fa7558515f47c0a33dcbc6df4
Naim Goksel Karacayli <ngokselk@gmail.com>
2026-03-11 16:27:35 -0400

    fix estimateIsotropicPower

--- mathtools/real_field_3d.cpp
+++ mathtools/real_field_3d.cpp
@@ -80,19 +80,24 @@ std::unique_ptr<double[]> RealField3D::estimateIsotropicPower(
         const RealField3D &mesh, int nbins
 ) {
     auto results = std::make_unique<double[]>(nbins);
+    auto weights = std::make_unique<double[]>(nbins);
     float amax = *std::max_element(mesh.dx, mesh.dx + 3);
     double knyq = MY_PI / amax, dkbin = knyq / nbins;
 
     // if (predeconvolve)  mesh.sharpen2();
-
-    #pragma omp parallel for
+    double *p = results.get(), *w = weights.get();
+    #pragma omp parallel for reduction(+:p[:nbins], w[:nbins])
     for (size_t i = 0; i < mesh.size_complex; ++i) {
         double k2, kz;
         mesh.getK2KzFromIndex(i, k2, kz);
         k2 = sqrt(k2);
         int binno = k2 / dkbin;
-        results[binno] += std::norm(mesh.field_k[i]) * mesh.invtotalvol;
+        p[binno] += std::norm(mesh.field_k[i]);
+        w[binno] += 1;
     }
+
+    for (int i = 0; i < nbins; ++i)
+        p[i] = mesh.invtotalvol / weights[i];
 
     return results;
 }
